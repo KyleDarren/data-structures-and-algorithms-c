06589925c5a85bc87235872f73b5f6ac3a7cc715
Kyle Darren <laguertakyledarren@gmail.com>
2024-10-30 09:39:26 +0800

    Completed

--- quick_sort.c
+++ quick_sort.c
@@ -1,50 +1,53 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-void quickSort(int *arr, size_t arr_size, int low, int high);
-int partition(int *arr, size_t arr_size);
+void quickSort(int *arr, int low, int high);
+int partition(int *arr, int low, int high);
 
 int main() {
-    int arr[] = {8, 7, 6, 5};
+    int arr[] = {10, 7, 8, 9, 1, 5};
     int arr_size = sizeof(arr)/sizeof(*arr);
-    quickSort(arr, arr_size, 0, arr_size);
-    printf("\n");
-    for (int i=0; i < arr_size; i++) {
-        printf("%d ", arr[i]);
-    }
-return 0;
+
+    quickSort(arr, 0, arr_size-1);
+    
+    return 0;
 }
 
-void quickSort(int *arr, size_t arr_size, int low, int high) {
-    if (arr_size > 1) {
-        // Set the last element as the pivot element
-        int pivot = arr[high-1];
-        int pivot_index;
-        int high_item = -1;
-        int high_index = -1;
-        for (int i=low; i < high; i++) {
-            if (arr[i] <= pivot && high_index != -1) {
-                int temp = arr[i];
-                arr[i] = arr[high_index];
-                arr[high_index] = temp;
-                pivot_index = high_index;
-                high_index = i;
-            } else if (arr[i] > pivot && high_item == -1) {
-                high_item = arr[i];
-                high_index = i;
-            }
-            
-        }
-        
-        quickSort(arr, pivot_index+1, low, pivot_index);
-        quickSort(arr, high-pivot_index, pivot_index, high);
-
+void quickSort(int *arr, int low, int high) {
+    // If the the sub-array size is 1, the code below will not be executed
+    // and the recursion loop will stop there.
+    if (low < high) {
+        int pivot_index = partition(arr, low, high);
+        quickSort(arr, low, pivot_index-1);
+        quickSort(arr, pivot_index+1, high);
     }
-    
 }
 
 // Returns a pivot index and modify the array
-int partition(int *arr, size_t arr_size) {
+int partition(int *arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
 
-    return 0;
+    for (int j=low; j < high; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+
+            // Switch element.
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    // Swith the pivot element and the (i+1)th element
+    int temp1 = arr[i+1];
+    arr[i+1] = arr[high];
+    arr[high] = temp1;
+
+    printf("\n");
+    for (int j=0; j < high+1; j++) {
+        printf("%d ", arr[j]);
+    }
+    
+    // Return the position of pivot element.
+    return i+1;
 }
