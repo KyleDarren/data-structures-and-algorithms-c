0a32ba8a005f11c536a4cd263699e482d89d9ecf
Kyle Darren <laguertakyledarren@gmail.com>
2024-11-15 16:30:25 +0800

    Modified

--- merge_sort.c
+++ merge_sort.c
@@ -1,92 +1,158 @@
-// UNFINISHED
-
 #include <stdio.h>
 #include <stdlib.h>
 
 // For debugging purposes
 void print_arr(int *arr, int arr_size) {
-    for (int i=0; i < arr_size; i++) {
+    for (int i = 0; i < arr_size; i++) {
         printf(" %d ", arr[i]);
     }
+    printf("\n");
 }
 
 int* merge_sort(int*, int);
 int* merge(int*, int*, int, int);
+void removeFirst(int **arr, int *arr_size);
+void insertBefore(int **arr, int *arr_size, int index, int item);
 
 int main() {
-
     // Creating a dynamic array
     int static_arr[] = {3, 6, 8, 0, 9, 4, 2};
     int arr_size = sizeof(static_arr) / sizeof(*static_arr);
-    int *arr = (int*)calloc(arr_size, sizeof(int));
-    for (int i=0; i < arr_size; i++) {
+    int *arr = (int*)malloc(arr_size * sizeof(int));
+    if (arr == NULL) {
+        perror("Memory allocation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < arr_size; i++) {
         arr[i] = static_arr[i];
     }
 
     int* result = merge_sort(arr, arr_size);
 
+    // Print the sorted array
     print_arr(result, arr_size);
 
-    // // Check array
-    // for (int i=0; i < arr_size; i++) {
-    //     printf("%d ", arr[i]);
-    // }
+    // Free allocated memory
+    free(result);
+    free(arr);
 
     return 0;
 }
 
 int* merge_sort(int *arr, int arr_size) {
     if (arr_size <= 1) {
-        return arr;
+        int *result = (int*)malloc(arr_size * sizeof(int));
+        if (result == NULL) {
+            perror("Memory allocation failed");
+            exit(EXIT_FAILURE);
+        }
+        if (arr_size > 0) {
+            result[0] = arr[0];
+        }
+        return result;
     }
 
     int left_arr_size = arr_size / 2;
-    // Make sure that the right_arr size is +1 than the left_arr if the arr_size is odd
-    int right_arr_size = arr_size / 2 + ((arr_size % 2 == 0) ? 0 : 1);
+    int right_arr_size = arr_size - left_arr_size;
 
     // Left of the array
-    int *left_arr = (int*)calloc(left_arr_size, sizeof(int));
-    for (int i=0; i < left_arr_size; i++) {
+    int *left_arr = (int*)malloc(left_arr_size * sizeof(int));
+    if (left_arr == NULL) {
+        perror("Memory allocation failed");
+        exit(EXIT_FAILURE);
+    }
+    for (int i = 0; i < left_arr_size; i++) {
         left_arr[i] = arr[i];
     }
-    
+
     // Right of the array
-    int *right_arr = (int*)calloc(right_arr_size, sizeof(int));
-    for (int i=0; i < right_arr_size; i++) {
-        right_arr[i] = arr[i+left_arr_size];
+    int *right_arr = (int*)malloc(right_arr_size * sizeof(int));
+    if (right_arr == NULL) {
+        perror("Memory allocation failed");
+        exit(EXIT_FAILURE);
     }
-    
+    for (int i = 0; i < right_arr_size; i++) {
+        right_arr[i] = arr[i + left_arr_size];
+    }
+
     int* left_arr_sort = merge_sort(left_arr, left_arr_size);
     int* right_arr_sort = merge_sort(right_arr, right_arr_size);
-
-    return merge(left_arr_sort, right_arr_sort, left_arr_size, right_arr_size);
 
     free(left_arr);
     free(right_arr);
 
+    return merge(left_arr_sort, right_arr_sort, left_arr_size, right_arr_size);
 }
 
 int* merge(int* left_arr, int* right_arr, int left_arr_size, int right_arr_size) {
-    int *result_arr = (int*)calloc(left_arr_size + right_arr_size, sizeof(int));
-
-    // Copy left_arr to the result_arr
-    for (int i=0; i < left_arr_size; i++) {
-        result_arr[i] = left_arr[i];
+    int *result_arr = (int*)malloc((left_arr_size + right_arr_size) * sizeof(int));
+    if (result_arr == NULL) {
+        perror("Memory allocation failed");
+        exit(EXIT_FAILURE);
     }
 
-    // FIX THIS
-    // for (int i=0; right_arr_size > 0; i++) {
-    //     // If rigt_arr[i] is less than result_arr[i]
-    //     if (right_arr[i] < result_arr[i]) {
-    //         for (int j=left_arr_size+i; j > 0; j--) {
-    //             result_arr[j] = result_arr[j-1];
-    //         }
-    //         result_arr[0] = right_arr[i];
-    //     }
-    //     // result_arr[left_arr_size+i] = right_arr[i];
-    //     right_arr_size--;
-    // }
+    int i = 0, j = 0, k = 0;
+
+    while (i < left_arr_size && j < right_arr_size) {
+        if (left_arr[i] <= right_arr[j]) {
+            result_arr[k++] = left_arr[i++];
+        } else {
+            result_arr[k++] = right_arr[j++];
+        }
+    }
+
+    // Copy remaining elements from left_arr
+    while (i < left_arr_size) {
+        result_arr[k++] = left_arr[i++];
+    }
+
+    // Copy remaining elements from right_arr
+    while (j < right_arr_size) {
+        result_arr[k++] = right_arr[j++];
+    }
+
+    free(left_arr);
+    free(right_arr);
 
     return result_arr;
+}
 
+void removeFirst(int **arr, int *arr_size) {
+    if (*arr_size <= 0 || *arr == NULL) {
+        return;
+    }
+
+    for (int i = 1; i < *arr_size; i++) {
+        (*arr)[i - 1] = (*arr)[i];
+    }
+
+    int *new_arr = realloc(*arr, (*arr_size - 1) * sizeof(int));
+    if (new_arr == NULL && *arr_size - 1 > 0) {
+        perror("Memory reallocation failed");
+        exit(EXIT_FAILURE);
+    }
+    *arr = new_arr;
+    (*arr_size)--;
+}
+
+void insertBefore(int **arr, int *arr_size, int index, int item) {
+    if (index < 0 || index > *arr_size) {
+        fprintf(stderr, "Error: Index out of bounds\n");
+        return;
+    }
+
+    int *new_arr = realloc(*arr, (*arr_size + 1) * sizeof(int));
+    if (new_arr == NULL) {
+        perror("Memory reallocation failed");
+        exit(EXIT_FAILURE);
+    }
+    *arr = new_arr;
+
+    for (int i = *arr_size; i > index; i--) {
+        (*arr)[i] = (*arr)[i - 1];
+    }
+
+    (*arr)[index] = item;
+    (*arr_size)++;
 }
