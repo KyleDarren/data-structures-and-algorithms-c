383a8319944d5b842ff0534e1f09cc5cd122feaf
Kyle Darren <laguertakyledarren@gmail.com>
2025-07-04 21:16:34 +0800

    Improved Implementation

--- qeueu.c
+++ qeueu.c
@@ -2,58 +2,134 @@
 #include <string.h>
 #include <stdlib.h>
 
+// struct Element {
+//     char data;
+//     struct Element *front;
+// };
+
+// struct Qeueu {
+//     struct Element *front;
+//     struct Element *rear;
+// };
+
+// void enqeueu(struct Element **rear, char data) {
+//     struct Element *newElement = (struct Element*) malloc(sizeof(struct Element));
+
+//     if (newElement == NULL)  return;
+
+//     newElement->data = data;
+//     newElement->front = *rear;
+//     *rear = newElement;
+// }
+
+// void deqeueu(struct Element *rear) {
+//     struct Element *toDelete, *prevElement;
+
+//     toDelete = rear;
+//     prevElement = rear;
+
+//     while (toDelete->front != NULL)
+//         toDelete = toDelete->front;
+
+//     while(prevElement->front != toDelete)
+//         prevElement = prevElement->front;
+    
+//     prevElement->front = NULL;
+//     free(toDelete);
+// }
+
+// int traverse(struct Element *rear) {
+//     struct Element *temp = rear;
+
+//     while (temp != NULL) {
+//         printf("%c->", temp->data);
+//         temp = temp->front;
+//     }
+// }
+
+// int main() {
+//     char data[] = {'a', 'b', 'c', 'd', 'e', 'f'};
+
+//     struct Element *rear = NULL;
+
+//     for (int i=0; i < sizeof(data)/sizeof(data[0]); i++)
+//         enqeueu(&rear, data[i]);
+
+//     enqeueu(&rear, 'g');
+//     deqeueu(rear);
+
+//     traverse(rear);
+
+//     return 0;
+// }
+
 struct Element {
-    char data;
-    struct Element *front;
+    int data;
+    struct Element *next;
+    struct Element *prev;
 };
 
-void enqeueu(struct Element **rear, char data) {
+struct Qeueu {
+    struct Element *front;
+    struct Element *rear;
+};
+
+void enqeueu(struct Qeueu *qeueu, int data) {
     struct Element *newElement = (struct Element*) malloc(sizeof(struct Element));
-
-    if (newElement == NULL)  return;
-
     newElement->data = data;
-    newElement->front = *rear;
-    *rear = newElement;
+    newElement->next = NULL;
+    newElement->prev = NULL;
+
+    if (qeueu->front == NULL) {
+        qeueu->front = newElement;
+        qeueu->rear = newElement;
+    } else {
+        newElement->next = qeueu->rear;
+        qeueu->rear->prev = newElement;
+        qeueu->rear = newElement;
+    }
 }
 
-void deqeueu(struct Element *rear) {
-    struct Element *toDelete, *prevElement;
-
-    toDelete = rear;
-    prevElement = rear;
-
-    while (toDelete->front != NULL)
-        toDelete = toDelete->front;
-
-    while(prevElement->front != toDelete)
-        prevElement = prevElement->front;
-    
-    prevElement->front = NULL;
-    free(toDelete);
+void deqeueu(struct Qeueu *qeueu) {
+    if (qeueu->front == NULL) {
+        printf("List is empty");
+    } else if (qeueu->front == qeueu->rear) {
+        free(qeueu->front);
+        qeueu->front = NULL;
+        qeueu->rear = NULL;
+    } else {
+        struct Element *toDelete;
+        toDelete = qeueu->front;
+        qeueu->front = toDelete->prev;
+        qeueu->front->next = NULL;
+        free(toDelete);
+    }
 }
 
-int traverse(struct Element *rear) {
-    struct Element *temp = rear;
+void traverse(struct Qeueu qeueu) {
+    struct Element *temp;
+    temp = qeueu.front;
 
     while (temp != NULL) {
-        printf("%c->", temp->data);
-        temp = temp->front;
+        printf("<-%d", temp->data);
+        temp = temp->prev;
     }
 }
 
 int main() {
-    char data[] = {'a', 'b', 'c', 'd', 'e', 'f'};
+    int num[5] = {4, 5, 2, 6, 1};
+    struct Qeueu qeueu;
+    qeueu.front = NULL;
+    qeueu.rear = NULL;
+    
+    for (int i=0; i < 5; i++) {
+        enqeueu(&qeueu, num[i]);
+    }
 
-    struct Element *rear = NULL;
+    deqeueu(&qeueu);
+    deqeueu(&qeueu);
+    enqeueu(&qeueu, 3);
 
-    for (int i=0; i < sizeof(data)/sizeof(data[0]); i++)
-        enqeueu(&rear, data[i]);
-
-    enqeueu(&rear, 'g');
-    deqeueu(rear);
-
-    traverse(rear);
-
+    traverse(qeueu);
     return 0;
 }
