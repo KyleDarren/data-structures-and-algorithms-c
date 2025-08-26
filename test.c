0dace216ade48bfd07c1ab2fadcc5b95d75b5f12
Darren <kyledarrendasco.laguerta24@bicol-u.edu.ph>
2025-08-26 10:16:36 +0800

    Removed executable files

--- test.c
+++ test.c
@@ -8,40 +8,52 @@ void print_arr(int *arr, int arr_size) {
     }
 }
 
-void removeFirst(int **arr, int arr_size) {
-    // Copy the array to a temporary array
-    int *temp_arr = (int*)calloc(arr_size, sizeof(int));
-    for (int i=0; i < arr_size; i++) {
-        temp_arr[i] = (*arr)[i];
+void merge (int arr[], int f1, int l1, int f2, int l2) {
+    int *temp_arr = (int*) malloc(sizeof(int)*(l2+1));
+    int i, j, k;
+    i = 0;
+    j = f1;
+    k = f2;
+
+    while (j <= l1 && k <= l2) {
+        if (arr[j] <= arr[k]) {
+            temp_arr[i++] = arr[j++];
+        } else {
+            temp_arr[i++] = arr[k++];
+        }
     }
 
-    // Reallocate the array
-    *arr = realloc(*arr, (arr_size-1) * sizeof(int));
-    for (int i=0; i < arr_size-1; i++) {
-        (*arr)[i] = temp_arr[i+1];
+    for (;j <= l1; j++) {
+        temp_arr[i++] = arr[j];
+    }
+
+    for (;k <= l2; k++) {
+        temp_arr[i++] = arr[k];
+    }
+
+    for (int t=0; t <= l2-f1; t++) {
+        arr[t+f1] = temp_arr[t];
     }
 }
 
-void insertBefore(int **arr, int arr_size, int index, int item) {
-    *arr = realloc(*arr, (arr_size+1)*sizeof(int));
-    for (int i=arr_size; i > index; i--) {
-        (*arr)[i] = (*arr)[i-1];
+void mergeSort(int arr[], int first, int last) {
+    if (first < last) {
+        int mid = (first + last) / 2;
+    
+        mergeSort(arr, first, mid);
+        mergeSort(arr, mid+1, last);
+        merge(arr, first, mid, mid+1, last);
     }
-    (*arr)[index] = item;
 }
 
 int main() {
-    int left_arr[] = {1, 3, 5, 7};
-    int *left_arr_dyn = (int*)calloc(4, sizeof(int));
-    for (int i=0; i < 4; i++) {
-        left_arr_dyn[i] = left_arr[i];
+    int arr[] = {3, 2, 6, 4, 1, 8, 7};
+    int n = sizeof(arr) / sizeof(arr[1]);
+
+    mergeSort(arr, 0, n-1);
+
+    for (int i=0; i < 7; i++) {
+        printf("%d->", arr[i]);
     }
-    
-    insertBefore(&left_arr_dyn, 4, 1, 5);
-
-    print_arr(left_arr_dyn, 5);
-
-
-    
     return 0;
 }
